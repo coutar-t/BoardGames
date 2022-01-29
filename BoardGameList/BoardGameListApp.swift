c1313a661b7dfac2618e0ede0aaafcc2fa30588a
thibaut.coutard <thibaut.coutard@thibautc.com>
2022-01-29 21:27:29 +0100

    Initial Commit

---
+++ BoardGameList/BoardGameListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameListApp.swift
+//  BoardGameList
+//
+//  Created by Thibaut Coutard on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
